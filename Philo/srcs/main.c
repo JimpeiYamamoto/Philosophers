8a04a551d59c1c860e1145fe9e6c8c6064e55bc0
JimpeiYamamoto <zinpei104@gmail.com>
2022-04-25 10:09:37 +0900

    philo_doingの途中

--- Philo/srcs/main.c
+++ Philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yjimpei <yjimpei@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 15:36:56 by yjimpei           #+#    #+#             */
-/*   Updated: 2022/04/25 01:25:00 by yjimpei          ###   ########.fr       */
+/*   Updated: 2022/04/25 10:05:32 by yjimpei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,53 @@
 void	*philo_action(void *arg)
 {
 	t_info	*info;
+	int		left;
+	int		right;
 
 	info = (t_info*)arg;
+	if (info->target % 2 == 0 && info->eat_cnt[info->target] == 0)
+		usleep(2);
+	while (1)
+	{
+		left = info->target;
+		right = (left + 1) % info->input->p_num;
+		//take left fork
+		pthread_mutex_lock(&(info->mutex_lst[left]));
+		if (info->is_taken[left] == FALSE)
+		{
+			info->is_taken[left] = TRUE;
+			info->philo_doing[info->target] = TAKE;
+			put_state(info, info->target);
+		}
+		pthread_mutex_unlock(&(info->mutex_lst[left]));
+		//take right fork
+		pthread_mutex_lock(&(info->mutex_lst[right]));
+		if (info->is_taken[right] == FALSE)
+		{
+			info->is_taken[right] = TRUE;
+			info->philo_doing[info->target] = TAKE;
+			put_state(info, info->target);
+		}
+		pthread_mutex_unlock(&(info->mutex_lst[right]));
+		//eat
+		if (info->is_taken[info->target] == TAKE &&
+			info->is_taken[left] == TRUE && info->is_taken[right] == TRUE)
+		{
+			//release fork
+			pthread_mutex_lock(&(info->mutex_lst[left]));
+			info->is_taken[left] = FALSE;
+			pthread_mutex_unlock(&(info->mutex_lst[left]));
+			pthread_mutex_lock(&(info->mutex_lst[left]));
+			info->is_taken[right] = FALSE;
+			pthread_mutex_unlock(&(info->mutex_lst[left]));
+			//start eat
+			info->philo_doing[info->target] = EAT;
+			put_state(info, info->target);
+			usleep(info->input->e_time);
+		}
+		//sleep
+		//think
+	}
 	return (NULL);
 }
 
